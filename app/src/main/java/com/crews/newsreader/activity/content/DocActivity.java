4463c555086c52264ad3f64c2b423c7b68007cf7
zia <jzljzljl@outlook.com>
2017-04-16 15:35:43 +0800

    安卓6.0以上doc界面沉浸 修复了图片加载库加载空url的闪退

--- app/src/main/java/com/crews/newsreader/activity/content/DocActivity.java
+++ app/src/main/java/com/crews/newsreader/activity/content/DocActivity.java
@@ -2,14 +2,21 @@ package com.crews.newsreader.activity.content;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,7 +47,31 @@ public class DocActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doc);
         init();
         bind();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setToolBar();
+        }
         getFromHttp();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setToolBar(){
+        Window window = this.getWindow();
+//设置透明状态栏,这样才能让 ContentView 向上
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+//需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//设置状态栏颜色
+        window.setStatusBarColor(Color.parseColor("#00000000"));
+        window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
+        ViewGroup mContentView = (ViewGroup) this.findViewById(Window.ID_ANDROID_CONTENT);
+        View mChildView = mContentView.getChildAt(0);
+        if (mChildView != null) {
+            //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 使其不为系统 View 预留空间.
+            ViewCompat.setFitsSystemWindows(mChildView, false);
+        }
     }
 
     private void init() {
